3021f243eaaf8464d74d89bac6de4dc95a4e1eb8
kaaaaaaaaaaaaaaa67 <alkawtherhussain789@gmail.com>
2026-06-25 16:15:07 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,39 @@
-‏import discord
-‏from discord.ext import commands, tasks
-‏import os
+import discord
+from discord.ext import commands, tasks
+import os
 
-‏TOKEN = os.getenv("TOKEN")
-‏VOICE_CHANNEL_ID = 1515629459045093389
+TOKEN = os.getenv("TOKEN")
+VOICE_CHANNEL_ID = 1515629459045093389
 
-‏intents = discord.Intents.default()
-‏bot = commands.Bot(command_prefix="!", intents=intents)
+intents = discord.Intents.default()
+bot = commands.Bot(command_prefix="!", intents=intents)
 
-‏async def join_voice():
-‏    channel = bot.get_channel(VOICE_CHANNEL_ID)
+async def join_voice():
+    channel = bot.get_channel(VOICE_CHANNEL_ID)
 
-‏    if channel is None:
-‏        print("Voice channel not found")
-‏        return
+    if channel is None:
+        print("Voice channel not found")
+        return
 
-‏    try:
-‏        if not bot.voice_clients:
-‏            await channel.connect()
-‏            print("Connected")
-‏    except Exception as e:
-‏        print(e)
+    try:
+        if not bot.voice_clients:
+            await channel.connect()
+            print("Connected")
+    except Exception as e:
+        print(e)
 
-‏@bot.event
-‏async def on_ready():
-‏    print(f"Logged in as {bot.user}")
-‏    await join_voice()
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+    await join_voice()
 
-‏@tasks.loop(minutes=1)
-‏async def keep_connected():
-‏    if not bot.voice_clients:
-‏        await join_voice()
+@tasks.loop(minutes=1)
+async def keep_connected():
+    if not bot.voice_clients:
+        await join_voice()
 
-‏@bot.event
-‏async def setup_hook():
-‏    keep_connected.start()
+@bot.event
+async def setup_hook():
+    keep_connected.start()
 
-‏bot.run(TOKEN)
+bot.run(TOKEN)
